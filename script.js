e9fb70f8b99ff607883679034c38abff1f70f9db
Salman Hasan <107566969+salmanhasan0@users.noreply.github.com>
2022-06-26 01:26:36 +0600

    jack dainels shop project
    jack dainels shop project

---
+++ script.js
@@ -0,0 +1,22 @@
+const header=document.querySelector(header);
+window.addEventListener("scroll",function(){
+    header.classList.toggle ("sticky",window.scrollY>0);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
